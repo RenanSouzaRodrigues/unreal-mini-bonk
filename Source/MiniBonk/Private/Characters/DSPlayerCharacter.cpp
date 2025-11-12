2f8c1c0b559effc6ae9769f9eb18f7e702f38305
Renan Souza <renan.souza@powerpush.com.br>
2025-11-12 19:47:01 -0300

    on development

--- Source/MiniBonk/Private/Characters/DSPlayerCharacter.cpp
+++ Source/MiniBonk/Private/Characters/DSPlayerCharacter.cpp
@@ -3,7 +3,10 @@
 #include "MiniBonk/Public/Characters/DSPlayerCharacter.h"
 #include "Macros/DSMacros.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 // =====================================================================================
@@ -11,16 +14,28 @@
 // =====================================================================================
 ADSPlayerCharacter::ADSPlayerCharacter() {
 	this->PrimaryActorTick.bCanEverTick = true;
+
+	this->GetCapsuleComponent()->SetCapsuleRadius(20.f);
+	
 	this->PlayerBody = this->CreateDefaultSubobject<UStaticMeshComponent>("Player Body");
 	this->PlayerBody->SetupAttachment(this->GetRootComponent());
 	this->PlayerBody->SetCollisionEnabled(ECollisionEnabled::Type::NoCollision);
 
 	this->PlayerCameraSpring = this->CreateDefaultSubobject<USpringArmComponent>("Player Camera Spring Arm");
 	this->PlayerCameraSpring->SetupAttachment(this->GetRootComponent());
+	this->PlayerCameraSpring->bUsePawnControlRotation = true;
 	
 	this->PlayerCamera = this->CreateDefaultSubobject<UCameraComponent>("Player Camera");
 	this->PlayerCamera->SetupAttachment(this->PlayerCameraSpring);
 	this->PlayerCamera->SetRelativeLocation(FVector(0.f, 0.f, 0.f));
+
+	if (const auto characterMovementComponent = this->GetCharacterMovement()) {
+		characterMovementComponent->GravityScale = 1.2f;
+		characterMovementComponent->MaxWalkSpeed = 700.f;
+		characterMovementComponent->AirControl = 0.1f;
+		characterMovementComponent->JumpZVelocity = 700.f;
+		characterMovementComponent->SetWalkableFloorAngle(46.f);
+	}
 }
 
 void ADSPlayerCharacter::BeginPlay() {
@@ -52,6 +67,22 @@ void ADSPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 	}
 
 	enhancedSubsystem->AddMappingContext(this->PlayerInputMappingContext, 0);
+
+	UEnhancedInputComponent* inputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (!inputComponent) {
+		DS_LOG_ERROR("Player Character Error: Enhanced input component is invalid");
+		return;
+	}
+
+	// Movement Input Action Binding
+	inputComponent->BindAction(this->MoveInputAction, ETriggerEvent::Triggered, this, &ADSPlayerCharacter::Move);
+
+	// Look Input Action Binding
+	inputComponent->BindAction(this->LookInputAction, ETriggerEvent::Triggered, this, &ADSPlayerCharacter::Look);
+
+	// Jump Input Action Binding
+	inputComponent->BindAction(this->JumpInputAction, ETriggerEvent::Started, this, &ADSPlayerCharacter::StartJump);
 }
 
 
@@ -62,4 +93,14 @@ void ADSPlayerCharacter::Move(const FInputActionValue& Value) {
 	const FVector2D movementValue = Value.Get<FVector2D>();
 	this->AddMovementInput(this->GetActorForwardVector(), movementValue.X);
 	this->AddMovementInput(this->GetActorRightVector(), movementValue.Y);
+}
+
+void ADSPlayerCharacter::Look(const FInputActionValue& Value) {
+	const FVector2D lookValue = Value.Get<FVector2D>();
+	this->AddControllerPitchInput(lookValue.Y);
+	this->AddControllerYawInput(lookValue.X);
+}
+
+void ADSPlayerCharacter::StartJump(const FInputActionValue& Value) {
+	this->Jump();
 }
